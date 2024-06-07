d0d8124b97363bbfff24e17ad4b63faac7f90482
pendyurinandrey <pendyurinandrey@gmail.com>
2024-06-07 22:39:18 +0500

    - Implemented reading file from os

--- downloader.go
+++ downloader.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/heroiclabs/nakama-common/runtime"
+	"hash/crc32"
+	"os"
+	"strconv"
 )
 
 const defaultType string = "core"
 const defaultVersion string = "1.0.0"
+
+const notFoundErrorCode = 5
 
 type DownloaderRequest struct {
 	Type    string `json:"type"`
@@ -36,5 +42,20 @@ func RpcFileDownloader(ctx context.Context, logger runtime.Logger, db *sql.DB, n
 		logger.Info("Unable to deserialize request %e", err)
 		return "", err
 	}
-	return "", nil
+	f, err := os.ReadFile(fmt.Sprintf("/%s/%s.json", req.Type, req.Version))
+	if err != nil {
+		return "{}", runtime.NewError("File not found", notFoundErrorCode)
+	}
+
+	crc32Table := crc32.MakeTable(crc32.IEEE)
+	fileCrc32 := strconv.FormatUint(uint64(crc32.Checksum(f, crc32Table)), 10)
+	var resp DownloaderResponse
+	if req.Hash != "" && fileCrc32 != req.Hash {
+		resp = DownloaderResponse{Type: req.Type, Version: req.Version, Hash: req.Hash, Content: nil}
+	} else {
+		resp = DownloaderResponse{Type: req.Type, Version: req.Version, Hash: req.Hash, Content: f}
+	}
+	respStr, err := json.Marshal(resp)
+	// TODO: Better error handling
+	return string(respStr[:]), err
 }
